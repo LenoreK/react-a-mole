134747b3cb99ed8c5ef918f15273172b43fbbe64
Lenore Kelly <lenorekelly2@gmail.com>
2022-03-22 13:53:09 -0400

    all files

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+import {useState} from 'react';
+import MoleContainer from './MoleContainer'
+
+
+function App() {
+  let [score, setScore] = useState(0)
+
+  const createMoleHill = () => {
+    let hills = []
+    for (let i = 0; i < 9; i++) {
+      hills.push(
+        <MoleContainer key={i} setScore={setScore} score={score} />
+      )
+    }
+    return (
+      <div>
+        {hills}
+      </div>
+    )
+  }
+
+  return (
+    <div className="App">
+      <h1>React-a-Mole!</h1>
+      {score}
+      {createMoleHill()}
+    </div>
+  );
+}
+
+export default App;
+
+/*import { useEffect } from 'react'
+import moleImg from '../mole.png'
+
+const Mole = (props) => {
+
+    useEffect(() => {
+        let randSeconds = Math.ceil(Math.random() * 10000)
+        let timer = setTimeout(() => {
+            props.toggle(false)
+        }, randSeconds)
+        return () => clearTimeout(timer)
+    })
+
+    return (
+        <div>
+            <img style={{'width': '30vw'}} src={moleImg} onClick={props.handleClick} />
+        </div>
+    )
+}
+
+export default Mole
+*/
---
+++ src/MoleContainer.js
@@ -0,0 +1,10 @@
+import molelmg from './public/RR-React-A-Mole-Images/mole.png'
+
+function MoleContainer(props) {
+    return (
+        <div>
+            <h2> Mole Container </h2>
+            <Mole />
+        </div>
+    )
+}
